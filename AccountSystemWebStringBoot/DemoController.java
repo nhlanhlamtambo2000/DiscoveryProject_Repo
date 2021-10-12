86dc9040df586c17fafdd1c439b62f008fb1564e
nhlanhlamtambo2000 <72925949+nhlanhlamtambo2000@users.noreply.github.com>
2021-10-12 03:52:50 +0200

    DemoController

---
+++ AccountSystemWebStringBoot/DemoController.java
@@ -0,0 +1,22 @@
+package za.ac.nwu.ac.we.sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+//import za.ac.nwu.ac.AccountSystemDomain.service.GeneralResponse;
+
+
+//@TestController
+/*public class DemoController
+{
+    //@GetMapping('/ping');
+    public GeneralResponse<String> ping(RequestParam(value - 'echo', defaultvalue - 'pong')String echo)
+    {
+        return new GeneralResponse<String>(true,echo);
+    }
+
+
+
+}
+
+ */
